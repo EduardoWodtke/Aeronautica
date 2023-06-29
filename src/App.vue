1d3fb257e606cc9461aa770ff18a772f5b5a69c2
EduardoWodtke <eduardorwodtkeifc@gmail.com>
2023-06-29 17:59:40 -0300

    imagens

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script setup>
   import slide from './components/slide.vue';
+  import navBar from './components/navBar.vue'; 
+  import footerHeader from './components/footerHeader.vue'; 
 </script>
 
 <template>
   
+  <navBar/>
+  
   <slide/>
+
+  <footerHeader/>
 
 </template>
 
