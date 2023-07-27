154128a53086a5faf62a90fa4878844bd44fd5f8
EduardoWodtke <eduardorwodtkeifc@gmail.com>
2023-07-26 22:18:13 -0300

    a

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -6,12 +6,12 @@
 <template>
  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>AeroCompany</title>
+  <title>AEROCOMPANY</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-      <a class="navbar-brand" href="#">AeroCompany</a>
+      <a class="navbar-brand" href="#">AEROCOMPANY</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
