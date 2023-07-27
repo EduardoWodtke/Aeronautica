812f137ffa8b694867ba77f83e29c822687edf08
EduardoWodtke <eduardorwodtkeifc@gmail.com>
2023-07-26 21:57:42 -0300

    a

--- src/components/BD.js
+++ src/components/BD.js
@@ -1,223 +1,262 @@
 const ArrayOpiniao = [
     {
-      id: 1,
-      pergunta: 'Qual é a maior cidade do Brasil?',
-      imagem: 0,
-      respostas: [
-        {
-          id: 1.1,
-          questao1: 'São Paulo',
-          resposta: true
-        },
-        {
-          id: 1.2,
-          questao1: 'Rio de Janeiro',
-          resposta: false
-        },
-        {
-          id: 1.3,
-          questao1: 'Belo Horizonte',
-          resposta: false
-        }
-      ]
+        id: 1,
+        pergunta: 'Qual é a sua companhia aérea preferida?',
+        imagem: 0,
+        respostas: [
+            {
+                id: 1.1,
+                questao1: 'LATAM',
+                resposta: false
+            },
+            {
+                id: 1.2,
+                questao1: 'Gol',
+                resposta: false
+            },
+            {
+                id: 1.3,
+                questao1: 'Azul',
+                resposta: false
+            },
+            {
+                id: 1.4,
+                questao1: 'Avianca',
+                resposta: false
+            },
+            {
+                id: 1.5,
+                questao1: 'Outra',
+                resposta: true
+            }
+        ]
     },
     {
-      id: 2,
-      pergunta: 'Qual é a capital da França?',
-      imagem: 0,
-      respostas: [
-        {
-          id: 2.1,
-          questao1: 'Londres',
-          resposta: false
-        },
-        {
-          id: 2.2,
-          questao1: 'Paris',
-          resposta: true
-        },
-        {
-          id: 2.3,
-          questao1: 'Madri',
-          resposta: false
-        }
-      ]
+        id: 2,
+        pergunta: 'Qual é o destino dos seus sonhos para uma viagem de avião?',
+        imagem: 0,
+        respostas: [
+            {
+                id: 2.1,
+                questao1: 'Paris',
+                resposta: false
+            },
+            {
+                id: 2.2,
+                questao1: 'Nova York',
+                resposta: false
+            },
+            {
+                id: 2.3,
+                questao1: 'Tóquio',
+                resposta: false
+            },
+            {
+                id: 2.4,
+                questao1: 'Sydney',
+                resposta: false
+            },
+            {
+                id: 2.5,
+                questao1: 'Outro',
+                resposta: true
+            }
+        ]
     },
     {
-      id: 3,
-      pergunta: 'Quantos estados têm os Estados Unidos?',
-      imagem: 0,
-      respostas: [
-        {
-          id: 3.1,
-          questao1: '48',
-          resposta: false
-        },
-        {
-          id: 3.2,
-          questao1: '50',
-          resposta: true
-        },
-        {
-          id: 3.3,
-          questao1: '52',
-          resposta: false
-        }
-      ]
+        id: 3,
+        pergunta: 'Qual é a sua opinião sobre o serviço de alimentação oferecido em voos?',
+        imagem: 0,
+        respostas: [
+            {
+                id: 3.1,
+                questao1: 'Excelente',
+                resposta: false
+            },
+            {
+                id: 3.2,
+                questao1: 'Bom',
+                resposta: false
+            },
+            {
+                id: 3.3,
+                questao1: 'Regular',
+                resposta: false
+            },
+            {
+                id: 3.4,
+                questao1: 'Ruim',
+                resposta: false
+            },
+            {
+                id: 3.5,
+                questao1: 'Péssimo',
+                resposta: true
+            }
+        ]
     },
     {
-      id: 4,
-      pergunta: 'Qual é a maior ilha do mundo?',
-      imagem: 0,
-      respostas: [
-        {
-          id: 4.1,
-          questao1: 'Groenlândia',
-          resposta: true
-        },
-        {
-          id: 4.2,
-          questao1: 'Austrália',
-          resposta: false
-        },
-        {
-          id: 4.3,
-          questao1: 'Nova Zelândia',
-          resposta: false
-        }
-      ]
+        id: 4,
+        pergunta: 'Qual é o fator mais importante para você na escolha de um voo?',
+        imagem: 0,
+        respostas: [
+            {
+                id: 4.1,
+                questao1: 'Preço',
+                resposta: false
+            },
+            {
+                id: 4.2,
+                questao1: 'Horário',
+                resposta: false
+            },
+            {
+                id: 4.3,
+                questao1: 'Conforto',
+                resposta: false
+            },
+            {
+                id: 4.4,
+                questao1: 'Companhia Aérea',
+                resposta: false
+            },
+            {
+                id: 4.5,
+                questao1: 'Outro',
+                resposta: true
+            }
+        ]
     },
     {
-      id: 5,
-      pergunta: 'Qual é o maior oceano do mundo?',
-      imagem: 0,
-      respostas: [
-        {
-          id: 5.1,
-          questao1: 'Oceano Atlântico',
-          resposta: false
-        },
-        {
-          id: 5.2,
-          questao1: 'Oceano Índico',
-          resposta: false
-        },
-        {
-          id: 5.3,
-          questao1: 'Oceano Pacífico',
-          resposta: true
-        }
-      ]
+        id: 5,
+        pergunta: 'Você prefere voos diretos ou com escalas?',
+        imagem: 0,
+        respostas: [
+            {
+                id: 5.1,
+                questao1: 'Diretos',
+                resposta: false
+            },
+            {
+                id: 5.2,
+                questao1: 'Com Escalas',
+                resposta: false
+            },
+            {
+                id: 5.3,
+                questao1: 'Tanto faz',
+                resposta: true
+            }
+        ]
     },
     {
-      id: 6,
-      pergunta: 'Qual é a montanha mais alta do mundo?',
-      imagem: 0,
-      respostas: [
-        {
-          id: 6.1,
-          questao1: 'Monte Everest',
-          resposta: true
-        },
-        {
-          id: 6.2,
-          questao1: 'Monte Kilimanjaro',
-          resposta: false
-        },
-        {
-          id: 6.3,
-          questao1: 'Monte Fuji',
-          resposta: false
-        }
-      ]
+        id: 6,
+        pergunta: 'Você costuma fazer check-in online ou presencialmente no aeroporto?',
+        imagem: 0,
+        respostas: [
+            {
+                id: 6.1,
+                questao1: 'Online',
+                resposta: false
+            },
+            {
+                id: 6.2,
+                questao1: 'Presencial',
+                resposta: false
+            },
+            {
+                id: 6.3,
+                questao1: 'Depende da situação',
+                resposta: true
+            }
+        ]
     },
     {
-      id: 7,
-      pergunta: 'Qual é o maior animal terrestre?',
-      imagem: 0,
-      respostas: [
-        {
-          id: 7.1,
-          questao1: 'Elefante Africano',
-          resposta: true
-        },
-        {
-          id: 7.2,
-          questao1: 'Girafa',
-          resposta: false
-        },
-        {
-          id: 7.3,
-          questao1: 'Rinoceronte Branco',
-          resposta: false
-        }
-      ]
+        id: 7,
+        pergunta: 'Para viagens internacionais, você prefere voar em classe econômica ou executiva?',
+        imagem: 0,
+        respostas: [
+            {
+                id: 7.1,
+                questao1: 'Classe Econômica',
+                resposta: false
+            },
+            {
+                id: 7.2,
+                questao1: 'Classe Executiva',
+                resposta: false
+            },
+            {
+                id: 7.3,
+                questao1: 'Não sei, nunca voei em classe executiva',
+                resposta: true
+            }
+        ]
     },
     {
-      id: 8,
-      pergunta: 'Em qual país está localizado o Taj Mahal?',
-      imagem: 0,
-      respostas: [
-        {
-          id: 8.1,
-          questao1: 'Índia',
-          resposta: true
-        },
-        {
-          id: 8.2,
-          questao1: 'Egito',
-          resposta: false
-        },
-        {
-          id: 8.3,
-          questao1: 'China',
-          resposta: false
-        }
-      ]
+        id: 8,
+        pergunta: 'Você já teve problemas com extravio de bagagem em viagens de avião?',
+        imagem: 0,
+        respostas: [
+            {
+                id: 8.1,
+                questao1: 'Sim',
+                resposta: false
+            },
+            {
+                id: 8.2,
+                questao1: 'Não',
+                resposta: false
+            },
+            {
+                id: 8.3,
+                questao1: 'Não, mas conheço alguém que teve',
+                resposta: true
+            }
+        ]
     },
     {
-      id: 9,
-      pergunta: 'Qual é a cidade do Vaticano?',
-      imagem: 0,
-      respostas: [
-        {
-          id: 9.1,
-          questao1: 'Roma',
-          resposta: false
-        },
-        {
-          id: 9.2,
-          questao1: 'Paris',
-          resposta: false
-        },
-        {
-          id: 9.3,
-          questao1: 'Cidade do Vaticano',
-          resposta: true
-        }
-      ]
+        id: 9,
+        pergunta: 'Como você avalia a pontualidade dos voos que você já pegou?',
+        imagem: 0,
+        respostas: [
+            {
+                id: 9.1,
+                questao1: 'Sempre pontuais',
+                resposta: false
+            },
+            {
+                id: 9.2,
+                questao1: 'Na maioria das vezes pontuais',
+                resposta: false
+            },
+            {
+                id: 9.3,
+                questao1: 'Algumas vezes atrasados, outras pontuais',
+                resposta: true
+            },
+            {
+                id: 9.4,
+                questao1: 'Na maioria das vezes atrasados',
+                resposta: false
+            },
+            {
+                id: 9.5,
+                questao1: 'Sempre atrasados',
+                resposta: false
+            }
+        ]
     },
     {
-      id: 10,
-      pergunta: 'Qual é o maior deserto do mundo?',
-      imagem: 0,
-      respostas: [
-        {
-          id: 10.1,
-          questao1: 'Deserto da Arábia',
-          resposta: false
-        },
-        {
-          id: 10.2,
-          questao1: 'Deserto de Gobi',
-          resposta: false
-        },
-        {
-          id: 10.3,
-          questao1: 'Deserto do Saara',
-          resposta: true
-        }
-      ]
+        id: 10,
+        pergunta: 'Você já teve uma experiência realmente ruim em um voo? Se sim, conte-nos!',
+        imagem: 0,
+        respostas: [
+            {
+                id: 10.1,
+                questao1: 'Sim',
+                resposta: false
+            }
+        ]
     }
-  ];
-  
+];
